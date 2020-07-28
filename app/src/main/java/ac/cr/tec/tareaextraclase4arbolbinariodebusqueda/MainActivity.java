322fad374701c2e0b55e40e97ed0a0601ff6aab2
lazhero <54720338+lazhero@users.noreply.github.com>
2020-07-28 07:07:47 -0600

    Initial Commit, AutoGenerated MainActivity

---
+++ app/src/main/java/ac/cr/tec/tareaextraclase4arbolbinariodebusqueda/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.cr.tec.tareaextraclase4arbolbinariodebusqueda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
