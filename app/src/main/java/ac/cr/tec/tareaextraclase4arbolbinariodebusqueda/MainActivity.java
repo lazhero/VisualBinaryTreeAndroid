ee3b8bd52995fd914ea6cdd6cba4a43f97249df5
lazhero <54720338+lazhero@users.noreply.github.com>
2020-07-28 21:52:21 -0600

    Updating the manifest and the git ignore

--- app/src/main/java/ac/cr/tec/tareaextraclase4arbolbinariodebusqueda/MainActivity.java
+++ app/src/main/java/ac/cr/tec/tareaextraclase4arbolbinariodebusqueda/MainActivity.java
@@ -1,14 +1,27 @@
 package ac.cr.tec.tareaextraclase4arbolbinariodebusqueda;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import ac.cr.tec.tareaextraclase4arbolbinariodebusqueda.BinaryTree.BinarySearchTree;
 
 public class MainActivity extends AppCompatActivity {
+    private BinarySearchTree tree;
+    private int count=0;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void init(View view){
+        Intent intent=new Intent(this,TreeActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
